bb18619ffaed3525f044781ac3550d427c2a0211
seblechernet <seble.t.chernet@gmail.com>
2018-09-19 17:01:51 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,79 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+//   for (int i=1;i<=15;i++)
+//   {
+//       System.out.println("Enter value " + i + ":");
+//       Scanner sc = new Scanner(System.in);
+//       double value = sc.nextDouble();
+//       double convertedvalue = value * 25.4;
+//       System.out.println(convertedvalue);
+//   }
+//    int i=1;
+//
+//   do {
+//
+//       System.out.println("Enter value " + i + ":");
+//       Scanner sc = new Scanner(System.in);
+//       double value = sc.nextDouble();
+//       double convertedvalue = value * 25.4;
+//       System.out.println(convertedvalue);
+//       i++;
+//   }while(i<=15);
+//
+//   i=1;
+//   while (i<=15)
+//   {
+//       System.out.println("Enter value  " + i + ":");
+//       Scanner sc = new Scanner(System.in);
+//       double value = sc.nextDouble();
+//       double convertedvalue = value * 25.4;
+//       System.out.println(convertedvalue);
+//       i++;
+//   }
+
+
+//        System.out.println("Enter the first value");
+//        Scanner keyboard = new Scanner(System.in);
+//        String myFirstString= keyboard.next();
+//        keyboard.nextLine();
+//        System.out.println("Enter the second value");
+//        String secondString = keyboard.nextLine();
+//        System.out.println("First String: " + myFirstString+ "\n Second String  " + secondString);
+
+//
+//        System.out.println("Enter your name: ");
+//        Scanner sc=new Scanner(System.in);
+//        String name=sc.nextLine();
+//        System.out.println("Hello " + name);
+//        System.out.println("Enter your age:");
+//        int age=sc.nextInt();
+//        System.out.println("The age entered is:" + age);
+//        System.out.println("are you married?: ");
+//        String status=sc.next();
+//
+//        if(status.equals("yes")){
+//            System.out.println("How many kids do you have?");
+//            int n=sc.nextInt();
+//            int i=1;
+//            while(i<=n){
+//                System.out.println("What is the name of your " + i + " Child");
+//                String childname = sc.nextLine();
+//                System.out.println("The name of your " + i + " child is " + childname);
+//                i++;
+//            }
+//
+//        }
+//        else
+//            System.out.println("Thank you good bye");
+
+
+            }
+    }
